09daf331c3ec0406915949f0abe8aedb4bab1a77
Willian Oliveira <88717345+williansw@users.noreply.github.com>
2025-08-02 16:03:13 -0300

    Load locate

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:babylon/app/base/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'app/base/dependences.dart';
+import 'app/core/constants/languages/resources.dart';
 import 'app/core/exceptions/exceptions.dart';
 
 Future<void> main() async {
   AppException.onError();
   WidgetsFlutterBinding.ensureInitialized();
+  R.load();
   setupLocator();
   runApp(const App());
 }
