3e78f089071fa493b9ef93399d9bece1a6c4767e
Willian Oliveira <88717345+williansw@users.noreply.github.com>
2025-08-03 12:01:37 -0300

    Firebase config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "br.com.lwl.babylon"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,9 +23,8 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "br.com.lwl.babylon"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
