c7e39e75f366cdfd44d37f88b7e80d1d9d5697f2
Willian Oliveira <88717345+williansw@users.noreply.github.com>
2025-08-03 11:49:22 -0300

    Add firebase  initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:babylon/app/base/app_widget.dart';
+import 'package:babylon/app/base/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/base/dependences.dart';
 import 'app/core/constants/languages/resources.dart';
@@ -7,6 +9,7 @@ import 'app/core/exceptions/exceptions.dart';
 Future<void> main() async {
   AppException.onError();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   R.load();
   setupLocator();
   runApp(const App());
